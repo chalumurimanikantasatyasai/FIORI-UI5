2d6a29cd17d9d989644d27fedec04c5a02756398
MANIKANTASATYASAI CHALUMURI <chalumurimanikantasatyasai@gmail.com>
2025-05-03 10:18:17 +0000

    initilize reposetery

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -7,122 +7,84 @@ sap.ui.define([
     return Controller.extend("project1.controller.View1", {
         onInit() {
         },
-        onAddition() {
-            let value1=parseInt(this.getView().byId("idVal1").getValue());
-            let value2=parseInt(this.getView().byId("idVal2").getValue());
-            let add=value1+value2;
+        // onAddition() {
+        //     let value1=parseInt(this.getView().byId("idVal1").getValue());
+        //     let value2=parseInt(this.getView().byId("idVal2").getValue());
+        //     let add=value1+value2;
 
 
-            MessageBox.alert("Total of two numbers " + add, {
-                title: "Alert",                                      // default
-                onClose: null,                                       // default
-                styleClass: "",                                      // default
-                actions: sap.m.MessageBox.Action.OK,                 // default
-                emphasizedAction: sap.m.MessageBox.Action.OK,        // default
-                initialFocus: null,                                  // default
-                textDirection: sap.ui.core.TextDirection.Inherit,    // default
-                dependentOn: null                                    // default
-            });
+        //     MessageBox.alert("Total of two numbers " + add, {
+        //         title: "Alert",                                      // default
+        //         onClose: null,                                       // default
+        //         styleClass: "",                                      // default
+        //         actions: sap.m.MessageBox.Action.OK,                 // default
+        //         emphasizedAction: sap.m.MessageBox.Action.OK,        // default
+        //         initialFocus: null,                                  // default
+        //         textDirection: sap.ui.core.TextDirection.Inherit,    // default
+        //         dependentOn: null                                    // default
+        //     });
 
-            this.getView().byId("idVal1").setValue("");
-            this.getView().byId("idVal2").setValue("");
+        //     this.getView().byId("idVal1").setValue("");
+        //     this.getView().byId("idVal2").setValue("");
 
-        },
-        onMulitiplication (){
-            let value1=parseInt(this.getView().byId("idVal1").getValue());
-            let value2=parseInt(this.getView().byId("idVal2").getValue());
-            let mul=value1*value2;
+        // },
+        // onMulitiplication (){
+        //     let value1=parseInt(this.getView().byId("idVal1").getValue());
+        //     let value2=parseInt(this.getView().byId("idVal2").getValue());
+        //     let mul=value1*value2;
 
-            // MessageToast.show("Multipule of two numbers " + mul, {
-            //     duration: 3000,                  // default
-            //     width: "15em",                   // default
-            //     my: "center bottom",             // default
-            //     at: "center bottom",             // default
-            //     of: window,                      // default
-            //     offset: "0 0",                   // default
-            //     collision: "fit fit",            // default
-            //     onClose: null,                   // default
-            //     autoClose: true,                 // default
-            //     animationTimingFunction: "ease", // default
-            //     animationDuration: 1000,         // default
-            //     closeOnBrowserNavigation: true   // default
-            // });
-            // MessageBox.confirm("Multipule of two numbers " + mul, {
-            //     title: "Confirm",                                    // default
-            //     onClose: null,                                       // default
-            //     styleClass: "",                                      // default
-            //     actions: [ sap.m.MessageBox.Action.OK,
-            //                sap.m.MessageBox.Action.CANCEL ],         // default
-            //     emphasizedAction: sap.m.MessageBox.Action.OK,        // default
-            //     initialFocus: null,                                  // default
-            //     textDirection: sap.ui.core.TextDirection.Inherit,    // default
-            //     dependentOn: null                                    // default
-            // });
+        //     MessageBox.show("Multipule of two numbers " + mul, {
+        //         icon: sap.m.MessageBox.Icon.NONE,                    // default
+        //         title: "",                                           // default
+        //         actions: sap.m.MessageBox.Action.OK,                 // default
+        //         emphasizedAction: sap.m.MessageBox.Action.OK,        // default
+        //         onClose: null,                                       // default
+        //         styleClass: "",                                      // default
+        //         initialFocus: null,                                  // default
+        //         textDirection: sap.ui.core.TextDirection.Inherit,    // default
+        //         dependentOn: null                                    // default
+        //     });
 
-            // MessageBox.error("Multipule of two numbers " + mul, {
-            //     title: "Error",                                      // default
-            //     onClose: null,                                       // default
-            //     styleClass: "",                                      // default
-            //     actions: sap.m.MessageBox.Action.CLOSE,              // default
-            //     emphasizedAction: null,                              // default
-            //     initialFocus: null,                                  // default
-            //     textDirection: sap.ui.core.TextDirection.Inherit,    // default
-            //     dependentOn: null                                    // default
-            // });
+           
+        //     this.getView().byId("idVal1").setValue("");
+        //     this.getView().byId("idVal2").setValue("");
 
-            // MessageBox.information("Multipule of two numbers " + mul, {
-            //     title: "Information",                                // default
-            //     onClose: null,                                       // default
-            //     styleClass: "",                                      // default
-            //     actions: sap.m.MessageBox.Action.OK,                 // default
-            //     emphasizedAction: sap.m.MessageBox.Action.OK,        // default
-            //     initialFocus: null,                                  // default
-            //     textDirection: sap.ui.core.TextDirection.Inherit,    // default
-            //     dependentOn: null                                    // default
-            // });
-
-            // MessageBox.show("Multipule of two numbers " + mul, {
-            //     icon: sap.m.MessageBox.Icon.NONE,                    // default
-            //     title: "",                                           // default
-            //     actions: sap.m.MessageBox.Action.OK,                 // default
-            //     emphasizedAction: sap.m.MessageBox.Action.OK,        // default
-            //     onClose: null,                                       // default
-            //     styleClass: "",                                      // default
-            //     initialFocus: null,                                  // default
-            //     textDirection: sap.ui.core.TextDirection.Inherit,    // default
-            //     dependentOn: null                                    // default
-            // });
-
-            // MessageBox.success("Multipule of two numbers " + mul, {
-            //     title: "Success",                                    // default
-            //     onClose: null,                                       // default
-            //     styleClass: "",                                      // default
-            //     actions: sap.m.MessageBox.Action.OK,                 // default
-            //     emphasizedAction: sap.m.MessageBox.Action.OK,        // default
-            //     initialFocus: null,                                  // default
-            //     textDirection: sap.ui.core.TextDirection.Inherit,    // default
-            //     dependentOn: null                                    // default
-            // });
-
-            MessageBox.warning("Multipule of two numbers " + mul, {
-                title: "Warning",                                    // default
-                onClose: null,                                       // default
-                styleClass: "",                                      // default
-                actions: sap.m.MessageBox.Action.ABORT,              // default
-                emphasizedAction: null,                              // default
-                initialFocus: null,                                  // default
-                textDirection: sap.ui.core.TextDirection.Inherit,    // default
-                dependentOn: null                                    // default
-            });
+        //    // console.log(total);
             
+        // }
 
+        onOperation (oEvent){
+            let value1 = parseInt(this.getView().byId("idVal1").getValue());
+            let value2 = parseInt(this.getView().byId("idVal2").getValue());
+            let operation = oEvent.getSource().getText();
+            let result;
 
+            switch (operation){
+                    case "addition":
+                    result = value1+value2
+                    this.getView().byId("idVal1").setValue("");
+                    this.getView().byId("idVal2").setValue("");
+                    break;
+                    case "subtraction":
+                    result = value1-value2
+                    this.getView().byId("idVal1").setValue("");
+                    this.getView().byId("idVal2").setValue("");
+                    break;
+                    case "multiplication":
+                    result = value1*value2
+                    this.getView().byId("idVal1").setValue("");
+                    this.getView().byId("idVal2").setValue("");
+                    break;
+                    case "division":
+                    result = value1/value2
+                    this.getView().byId("idVal1").setValue("");
+                    this.getView().byId("idVal2").setValue("");
+                    break;
+                    default:
+                        result = "Unknown operation";
 
-            this.getView().byId("idVal1").setValue("");
-            this.getView().byId("idVal2").setValue("");
-
-           // console.log(total);
-            
+            }
+            MessageBox.show("The result of two numbers is "     + result);
         }
 
     });
