5f9678e55e567a68eae5d6b2a99c0bcb31a5204a
MANIKANTASATYASAI CHALUMURI <chalumurimanikantasatyasai@gmail.com>
2025-05-04 05:06:12 +0000

    secound commit

--- studentinfo/webapp/controller/View1.controller.js
+++ studentinfo/webapp/controller/View1.controller.js
@@ -6,28 +6,28 @@ sap.ui.define([
     return Controller.extend("org.inedxit.studentinfo.controller.View1", {
         onInit() {
         },
-        onGetDetails(){
+        onGetDetails() {
 
             let studata = {
-                "studentid":"001",
-                "name":"Manikanta",
-                "class":"10",
-                "section":"A"
+                "studentid": "001",
+                "name": "Manikanta",
+                "class": "10",
+                "section": "A"
             };
 
-           // let jModel = new sap.ui.model.json.JSONModel(studata);
-           let jModel=new sap.ui.model.json.JSONModel();
-           jModel.setData(studata);
-           this.getView().setModel(jModel,"stdModel");
-           let sId = this.getView.getModel("stdModel").getProperty("/studentid");
-           let sname = this.getView.getModel("stdModel").getProperty("/name");
-           let sclass = this.getView.getModel("stdModel").getProperty("/class");
-           let ssec = this.getView.getModel("stdModel").getProperty("/section");
-           
-           this.getView().byId("stdId").setValue(sId);
-           this.getView().byId("stdName").setValue(sname);
-           this.getView().byId("stdcla").setValue(sclass);
-           this.getView().byId("stdSec").setValue(ssec);
-        } 
+            // let jModel = new sap.ui.model.json.JSONModel(studata);
+            let jModel = new sap.ui.model.json.JSONModel();
+            jModel.setData(studata);
+            this.getView().setModel(jModel, "stdModel");
+            let sId = this.getView.getModel("stdModel").getProperty("/studentid");
+            let sname = this.getView.getModel("stdModel").getProperty("/name");
+            let sclass = this.getView.getModel("stdModel").getProperty("/class");
+            let ssec = this.getView.getModel("stdModel").getProperty("/section");
+
+            this.getView().byId("stdId").setValue(sId);
+            this.getView().byId("stdName").setValue(sname);
+            this.getView().byId("stdcla").setValue(sclass);
+            this.getView().byId("stdSec").setValue(ssec);
+        }
     });
 });
